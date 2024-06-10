44bfbdd3410bc1f3c8a417ad3c484c3b9c78d1c2
rithishace <66012621+rithishace@users.noreply.github.com>
2024-06-10 14:51:40 +0530

    Config Project for development

--- school_project/settings.py
+++ school_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-$(w(w1tvvduorhh&x9$y6^pthf2wjbtii^yr$fhkd*4fxw&7f8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meclassroom187.heroku.com']
 
 
 # Application definition
